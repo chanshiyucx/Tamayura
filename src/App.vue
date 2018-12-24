56fc2a18808e25472d4328cb64191505e1598b96
chanshiyu <1124590931@qq.com>
2018-12-24 22:04:17 +0700

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="container">
+      <div class="resume">
+        <Sidebar />
+        <article>555</article>
+      </div>
+      <Footer />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import { Sidebar, Footer } from './components'
 export default {
-  name: "app",
+  name: 'app',
   components: {
-    HelloWorld
+    Sidebar,
+    Footer
   }
-};
+}
 </script>
 
 <style lang="less">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './index.less';
 </style>
